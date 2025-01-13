aeb63eefd6b18dd1f20092c555f6a56cf9ed712b
Elliot Courant <me@elliotcourant.dev>
2025-01-13 13:06:58 -0600

    chore: Fixing test on windows

--- monetary_test.go
+++ monetary_test.go
@@ -14,7 +14,7 @@ func TestGetLConv(t *testing.T) {
 	t.Run("en_US", func(t *testing.T) {
 		lconv, err := locale.GetLConv("en_US")
 		assert.NoError(t, err, "should not return an error for en_US")
-		assert.Equal(t, []byte("USD "), lconv.IntCurrSymbol)
+		assert.Subset(t, lconv.IntCurrSymbol, []byte("USD"))
 		assert.Equal(t, []byte("."), lconv.DecimalPoint)
 		assert.Equal(t, []byte(","), lconv.ThousandsSep)
 		assert.Equal(t, []byte("$"), lconv.CurrencySymbol)
